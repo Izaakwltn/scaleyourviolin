acd6903a5dcd2722c3fea3f9f5c67f8581ebc4c2
Izaakwltn <75645156+Izaakwltn@users.noreply.github.com>
2022-01-28 11:54:40 -0700

    anonymized directory path

--- build-site.lisp
+++ build-site.lisp
@@ -7,7 +7,7 @@
   (make-instance 'hunchentoot:easy-acceptor
 		 :port 4242
 		 :document-root
-		 #p"/home/izaakwalton/programming/lisp-projects/scaleyourviolin/www/"))
+		 #p"/path/to/scaleyourviolin/www/"))
 
 (defun launch ()
   (hunchentoot::start *server*))
